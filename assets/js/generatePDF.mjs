cd27f98840bba2971ef6a8a91757eb7e64d55824
Erwan <erwan.gaudicheau@campus-espl.org>
2025-01-25 09:17:28 +0100

    🐛 PDF Responsive
    Adapte la taille des éléments du PDF

--- assets/js/generatePDF.mjs
+++ assets/js/generatePDF.mjs
@@ -1,30 +1,32 @@
 export function generatePDF() {
-    var downloadButton = document.querySelector("#download");
+    const downloadButton = document.querySelector("#download");
     if (downloadButton) {
         downloadButton.style.display = 'none';
     }
 
     var element = document.querySelector(".tab").cloneNode(true);
-    element.style.width = '42.08vw';
+    element.style.width = '100%';
     element.style.height = 'auto';
     element.style.border = 'none';
     var logo = document.querySelector('header img').cloneNode(true);
-    logo.style.width = '8.39vw';
-    logo.style.height = '2.92vw';
+    logo.style.width = '20%';
+    logo.style.height = 'auto';
     logo.style.display = 'block';
-    logo.style.position = 'absolute';
-    logo.style.top = '4.3vw';
-    logo.style.margin = 'auto';
+    logo.style.margin = '10% auto';
     element.insertBefore(logo, element.firstChild);
 
     html2pdf(element, {
-        margin:       [10, 10, 10, 10],
-        filename:     'tableau_amortissement.pdf',
-        image:        { type: 'jpeg', quality: 0.98 },
-        html2canvas:  { scale: 2, useCORS: true },
-        jsPDF:        { unit: 'mm', format: 'a4', orientation: 'portrait' },
-        pagebreak:    { mode: ['avoid-all', 'css', 'legacy'] }
-    }).then(function() {
+        margin: [10, 10, 10, 10],
+        filename: 'tableau_amortissement.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2, useCORS: true },
+        jsPDF: { 
+            unit: 'mm', 
+            format: 'a4',
+            orientation: 'portrait' 
+        },
+        pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
+    }).then(function () {
         if (downloadButton) {
             downloadButton.style.display = 'block';
         }
